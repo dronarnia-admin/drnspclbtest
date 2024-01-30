2340b9a8358de94a3f8c01e318155fc23de9f976
dronarnia-admin <115626469+dronarnia-admin@users.noreply.github.com>
2024-01-30 21:02:54 +0200

    Changed background color ro black index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,12 @@ export default function Home() {
       <h1>Drone Space Lab</h1>
       <h2>Contact Us</h2>
     <p>If you have any questions, feel free to contact us at: <a href="mailto:drnfactory@gmail.com">drnfactory@gmail.com</a> </p>
+       <style>
+        body {
+            background-color: black;
+            color: white; /* Optionally, you can set the text color to white for better readability */
+        }
+    </style>
     </main>
   );
 }
