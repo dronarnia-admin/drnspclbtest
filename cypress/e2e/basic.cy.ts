b4c18884da3e51412b21684d36206a8a0ce690a3
dronarnia-admin <115626469+dronarnia-admin@users.noreply.github.com>
2024-01-30 20:42:51 +0200

    Changed main page removed clicker counter

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -5,11 +5,8 @@ describe('empty spec', () => {
 
   it('display header text', () => {
     cy.get('h1')
-    .contains('Hello World!', { matchCase: false })
+    .contains('Drone Space Labs', { matchCase: false })
   })
 
-  it('displays Click button', () => {
-    cy.get('button')
-    .contains('Clicks');
-  })
+
 })
