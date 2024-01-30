eb8040644be5d4b9049721e193bc45deace1580f
dronarnia-admin <115626469+dronarnia-admin@users.noreply.github.com>
2024-01-30 20:54:33 +0200

    Added email addres to index.tsx
    Added email addres

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,9 +6,9 @@ export default function Home() {
     <main>
       <Title>Drone Space Lab</Title>
       <h1>Drone Space Lab</h1>
-      <p>
-        The web site is under development. Plese come back later or contact us 
-      </p>
+      <h2>Contact Us</h2>
+    <p>If you have any questions, feel free to contact us at:</p>
+    <p>Email: <a href="mailto:drnfactory@gmail.com">drnfactory@gmail.com</a></p>
     </main>
   );
 }
