f49388ed3ca0769026d350985adf10083eec1cc5
dronarnia-admin <115626469+dronarnia-admin@users.noreply.github.com>
2024-01-30 20:55:29 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,8 +7,7 @@ export default function Home() {
       <Title>Drone Space Lab</Title>
       <h1>Drone Space Lab</h1>
       <h2>Contact Us</h2>
-    <p>If you have any questions, feel free to contact us at:</p>
-    <p>Email: <a href="mailto:drnfactory@gmail.com">drnfactory@gmail.com</a></p>
+    <p>If you have any questions, feel free to contact us at: <a href="mailto:drnfactory@gmail.com">drnfactory@gmail.com</a> </p>
     </main>
   );
 }
